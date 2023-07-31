8893d33c73534127224fcc0b4a730ec941612de3
DemonSlayer111 <84251406+DemonSlayer111@users.noreply.github.com>
2023-07-30 23:51:29 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+function replaceStringObject(props) {
+  function replace(matched) {
+    const args = matched.substring(1, matched.length - 1).split(".");
+
+    let workingValue = props;
+    let returnValue = matched;
+
+    args.forEach((value) => {
+      workingValue = workingValue?.[value];
+      switch (typeof workingValue) {
+        case "string":
+          returnValue = workingValue;
+          break;
+        case "number":
+        case "bigint":
+          returnValue = workingValue.toString();
+          break;
+      }
+    });
+
+    return returnValue;
+  }
+  replace.props = props;
+  return replace;
+}
+
+function stringObject(string, props) {
+  const stringObjectRegex = /({[^.{}]+(\.[^.{}]+)+})/g;
+
+  const updatedString = string.replaceAll(
+    stringObjectRegex,
+    replaceStringObject(props)
+  );
+
+  return updatedString;
+}
+
+module.exports = stringObject;
