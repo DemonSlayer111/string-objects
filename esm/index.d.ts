bf51e0ae1df5ecf4ebfedb84fd25ffed636e6028
DemonSlayer111 <84251406+DemonSlayer111@users.noreply.github.com>
2023-08-13 20:57:31 -0400

    update typedef

--- esm/index.d.ts
+++ esm/index.d.ts
@@ -1,5 +1,15 @@
-type Schema = string | number | bigint | Schema[] | {
-    [key: string]: Schema;
-};
-declare function stringObject(string: string, props: Exclude<Schema, string | bigint | number>): string;
+type Schema =
+  | string
+  | number
+  | bigint
+  | Schema[]
+  | {
+      [key: string]: Schema;
+    };
+declare function stringObject(
+  string: string,
+  props: Exclude<Schema, string | bigint | number>
+): string;
 export default stringObject;
+
+export type Schema = Exclude<Schema, string | number | bigint>;
