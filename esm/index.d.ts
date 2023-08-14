6bed52a9788c6eea455aac60cdae87053101b2e6
DemonSlayer111 <84251406+DemonSlayer111@users.noreply.github.com>
2023-08-13 20:48:29 -0400

    fix typnig error

---
+++ esm/index.d.ts
@@ -0,0 +1,5 @@
+type Schema = string | number | bigint | Schema[] | {
+    [key: string]: Schema;
+};
+declare function stringObject(string: string, props: Exclude<Schema, string | bigint | number>): string;
+export default stringObject;
